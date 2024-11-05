a1f3e4e37febf9b74854ac38d27288cfefb904f4
Winston Chang <winston@posit.co>
2024-11-04 19:52:01 -0600

    Update packages

--- flake.nix
+++ flake.nix
@@ -1,18 +1,33 @@
 {
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-23.11"; };
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-24.05";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in rec {
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
-              (python310.withPackages
-                (ps: with ps; [ ipython jupyterlab numpy pandas matplotlib ]))
-            ];
+          buildInputs = with pkgs; [
+            (python312.withPackages (
+              ps: with ps; [
+                pip
+                ipython
+                jupyterlab
+                numpy
+                pandas
+                matplotlib
+              ]
+            ))
+          ];
           shellHook = ''
             if [ ! -d venv ]; then
               echo "Virtual environment not found. Creating one..."
@@ -23,5 +38,6 @@
             echo Hello, welcome to the notebook dev environment!
           '';
         };
-      });
+      }
+    );
 }
